634ca77ee907d09801ece204215426a91ad8e751
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2023-10-03 01:24:15 +0900

    update: RestaurantController
    update: OrderModel
    
    update: KitchenService
    
    update: フォーマット修正

---
+++ src/main/java/com/example/springwebflux/service/KitchenService.java
@@ -0,0 +1,17 @@
+package com.example.springwebflux.service;
+
+import com.example.springwebflux.model.Order;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@Service
+public class KitchenService {
+
+    public Mono<Order> prepareDish(Order order) {
+        return Mono.just(order)
+                .delayElement(Duration.ofSeconds(5))
+                .doOnNext(o -> System.out.println("Preparing dish for " + o.getCustomerName()));
+    }
+}
